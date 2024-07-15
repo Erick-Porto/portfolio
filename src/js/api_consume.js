27fa5237ec5e9650966bf910dcc96b4d98e4570e
Erick Porto <srporto.erick@gmail.com>
2024-07-15 18:42:46 -0300

    githubapi

---
+++ src/js/api_consume.js
@@ -0,0 +1,30 @@
+function getGitHubAPI(){
+    fetch('https://api.github.com/users/erick-porto/repos')
+    .then(async res => {
+        if(!res.ok){
+            throw new Error(res.status);
+        }
+
+        const data = await res.json();
+        data.map( i => {
+
+            document.getElementById('portfolio').innerHTML = `
+            <div class="card swiper-slide">
+                <div class="image-content">
+                    <div class="card-image">
+                            <img src="${i.html_url}/blob/main/src/images/logo.jpg?raw=true" class="card-img">
+                    </div>
+                </div>
+
+                <div class="card-content">
+                    <h2 class="project-name">${i.name}</h2>
+                    <span class='project-language'>${i.language}</span>
+                    <p class="project-date">${ Intl.DateTimeFormat('pt-br').format(new Date(i.created_at)) }</p>
+
+                    <a href="${i.html_url}" target="_blank" class="project-repos-url">Ver Repositório</a>
+                </div>
+            </div>`;
+        })
+    }) 
+}
+getGitHubAPI()
---
+++ src/js/slider.js
@@ -0,0 +1,20 @@
+ 
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
